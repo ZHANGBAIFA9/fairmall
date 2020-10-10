f7505956ecc9ba88eb287f6af24fa50d5721edfe
ZHANGBAIFA <1350862999@qq.com>
2020-10-10 11:08:06 +0800

    集市商城-用户组织角色、pom文件、配置文件 /ZHANGBAIFA

---
+++ fairmall-organization/src/main/java/com/fairmall/FairmallOrganizationApplication.java
@@ -0,0 +1,19 @@
+package com.fairmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Version 1.0
+ * @Author ZHANGBAIFA
+ * @Date 2020/10/9 15:33
+ * @Description:
+ */
+@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker
+public class FairmallOrganizationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FairmallOrganizationApplication.class, args);
+    }
+}
