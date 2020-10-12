22e6047cd24d3b0a78787a7b2cd3410891d6881b
ZHANGBAIFA <1350862999@qq.com>
2020-10-12 14:35:42 +0800

    集市商城-通用模块-用户认证拦截 /ZHANGBAIFA

---
+++ fairmall-common/src/main/java/com/fairmall/interceptor/UserInterceptor.java
@@ -0,0 +1,24 @@
+package com.fairmall.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * @Version 1.0
+ * @Author ZHANGBAIFA
+ * @Date 2020/10/12 9:11
+ * @Description:
+ */
+@Slf4j
+@Configuration
+@ComponentScan(basePackages = { "com.fairmall.fairmall-auth.client" })
+public class UserInterceptor implements HandlerInterceptor {
+    @Value("")
+    private boolean authEnabled;
+
+
+
+}
