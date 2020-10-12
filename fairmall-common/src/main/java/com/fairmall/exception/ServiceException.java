387e7d3d82c721c1584991cab3b895c07ab1e21d
ZHANGBAIFA <1350862999@qq.com>
2020-10-12 15:10:03 +0800

    集市商城-通用模块-异常类 /ZHANGBAIFA

---
+++ fairmall-common/src/main/java/com/fairmall/exception/ServiceException.java
@@ -0,0 +1,13 @@
+package com.fairmall.exception;
+
+/**
+ * @Version 1.0
+ * @Author ZHANGBAIFA
+ * @Date 2020/10/12 15:08
+ * @Description: 业务异常类进行规范
+ */
+public class ServiceException extends BaseException{
+
+    //TODO 对业务异常的返回码进行校验，规范到一定范围内
+
+}
